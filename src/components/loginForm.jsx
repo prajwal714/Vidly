a000f7e9e6a2e03115e272b8b55b5b57040a7de3
prajwal714 <prajwal714singh@gmail.com>
2019-09-19 21:54:43 +0530

    commit

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -4,13 +4,31 @@ class LoginForm extends Component {
 
   //to access a DOM element, we need to give it a reference, we create a ref object 
   username = React.createRef();
+  state={
+    account: {
+      username: "",
+      password: ""
+    }
+  }
+
+  // componentDidMount() {
+  //   this.username.current.focus();
+  // }
+  
 
   handleSubmit = e => {
     e.preventDefault();
     const username = this.username.current.value; //returns the actual DOM element
     console.log("submitted form", username);
   };
+
+  handleChange=({currentTarget: input})=>{
+    const account={...this.state.account};
+    account[input.name]=input.value;
+    this.setState({account});
+  }
   render() {
+    let {account}=this.state;
     return (
       <div>
         <h1>Login Form</h1>
@@ -19,15 +37,18 @@ class LoginForm extends Component {
             <label htmlFor="username">Username</label>
             <input
               autoFocus
+              value={account.username}
+              onChange={this.handleChange}
               ref={this.username}
               id="username"
+              name="username"
               type="text"
               className="form-control"
             />
           </div>
           <div className="form-group">
             <label htmlFor="password">Password</label>
-            <input id="password" type="text" className="form-control" />
+            <input id="password" name="password" type="text" value={account.password} onChange={this.handleChange} className="form-control" />
           </div>
           <button className="btn btn-primary">Login</button>
         </form>
